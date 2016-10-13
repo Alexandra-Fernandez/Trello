0e6a353d50b22dec67570d9bf1ba893c6e3993ea
Alexandra <alexandrafernandezquispe@gmail.com>
2016-10-13 10:52:59 -0500

    version 0.0.2

--- js/app.js
+++ js/app.js
@@ -7,11 +7,32 @@ window.addEventListener("load",function(){
     var subContenedor = document.getElementById("sub-contenedor");
 
 	spanAñadir.addEventListener("click",imprimirFormulario);
+    boton.addEventListener("click",imprimirNombreLista);
 
     function imprimirFormulario(){
         
         formulario.classList.remove("aparecer");
         spanAñadir.remove();
         input.focus();
+    };
+    
+    function imprimirNombreLista(e){
+        e.preventDefault();
+        
+        formulario.remove();
+        
+        var nombreLista = document.createElement("div");
+            nombreLista.classList.add("titulo-lista");
+            nombreLista.innerHTML = input.value;
+        
+        var crearTajeta = document.createElement("a");
+            crearTajeta.setAttribute("href", "#");
+            crearTajeta.classList.add("nueva-tarjeta");
+            crearTajeta.innerText = "Añadir una tarjeta";
+        
+        subContenedor.appendChild(nombreLista);
+        subContenedor.appendChild(crearTajeta);
+        
+        input.value = "";
     };
 });
