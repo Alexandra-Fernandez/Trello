452905041875150bff3235548263f93087829235
Alexandra <alexandrafernandezquispe@gmail.com>
2016-10-18 11:18:53 -0500

    trello

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.addEventListener("load",function(){
 	var boton = document.getElementById("boton");
-	var spanAñadir = document.getElementById("añade");
+	var spanAñadir = document.getElementById("anade");
 	var input = document.getElementById("input");
 	var formulario = document.getElementById("formulario");
     var contenedor = document.getElementById("contenedor");
@@ -11,70 +11,86 @@ window.addEventListener("load",function(){
 
     function imprimirFormulario(){
         
-        formulario.classList.remove("aparecer");
-        spanAñadir.remove();
+        formulario.classList.remove("ocultar");
+        spanAñadir.classList.add("ocultar");
         input.focus();
     };
     
     function imprimirNombreLista(e){
         e.preventDefault();
         
-        formulario.remove();
+        
+        var caja = document.createElement("div");
+            caja.classList.add("caja");
         
         var nombreLista = document.createElement("div");
-            nombreLista.classList.add("titulo-lista");
-            nombreLista.innerHTML = input.value;
+            nombreLista.classList.add("tituloLista");
+            nombreLista.innerText = input.value;
         
-        var crearTajeta = document.createElement("a");
-            crearTajeta.setAttribute("href", "#");
-            crearTajeta.classList.add("nueva-tarjeta");
-            crearTajeta.innerText = "Añadir una tarjeta";
+        var crearTarjeta = document.createElement("div");
+            crearTarjeta.classList.add("tarjeta");
         
-        subContenedor.appendChild(nombreLista);
-        subContenedor.appendChild(crearTajeta);
+        var tarjeta = document.createElement("a")
+            tarjeta.setAttribute("href", "#");
+            tarjeta.innerText = "Añadir una tarjeta";
         
-        input.value = "";
+            crearTarjeta.appendChild(tarjeta);
         
-        var nuevasTarjetas = document.createElement("div");
-        crearTajeta.insertBefore(nuevasTarjetas, crearTajeta.children[3]);
+        subContenedor.appendChild(caja);
+        caja.appendChild(nombreLista);
+        caja.appendChild(crearTarjeta);
         
+        //spanAñadir.classList.remove("aparecer");
+        //contenedor.appendChild(subContenedor);
+        
+        
+//        subContenedor.appendChild(nombreLista);
+//        subContenedor.appendChild(crearTarjeta);
+           
+        input.value = ""; 
         
         divNuevo();
         
-        crearTajeta.addEventListener("click",function(){
+        crearTarjeta.addEventListener("click", function(e){
+           e.preventDefault();
             
-            crearTajeta.style.display="none";
+            crearTarjeta.style.display="none";
             
-            var nuevoForm = document.createElement("form");
+            var nuevoForm = document.createElement("div");
+                nuevoForm.classList.add("nuevaTarjeta");
                 subContenedor.appendChild(nuevoForm);
             
             var nuevoTextArea = document.createElement("textarea");
+                nuevoTextArea.classList.add("textarea")
                 nuevoForm.appendChild(nuevoTextArea);
             
             var nuevoBoton = document.createElement("button");
+                nuevoBoton.classList.add("nuevoboton");
                 nuevoBoton.innerText = "Guardar";
                 nuevoForm.appendChild(nuevoBoton);
+                caja.appendChild(nuevoForm);
+                
             
-            nuevoBoton.addEventListener("click",function(e){
+            nuevoBoton.addEventListener("click", function(e){
                 e.preventDefault();
                 
-                nuevoForm.style.display="none";
-                
-                var newTarjeta = document.createElement("span");
+                crearTarjeta.style.display="block";
+                var newTarjeta = document.createElement("div");
+                    newTarjeta.classList.add("tituloLista")
                     newTarjeta.innerText = nuevoTextArea.value;
-                    newTarjeta.style.display="block";
-                    nuevasTarjetas.appendChild(newTarjeta);
-                    
-                    subContenedor.appendChild(crearTajeta);
-                    crearTajeta.style.display = "inline-block";
+                    nuevoForm.style.display="none";
+                   
+                    caja.appendChild(newTarjeta);
+                    caja.appendChild(crearTarjeta);
             });
         });
     };
-    function divNuevo(){
+     function divNuevo(){
+         
         var nuevoDiv = document.createElement("div");
         contenedor.appendChild(nuevoDiv);
-        nuevoDiv.appendChild(spanAñadir);
         nuevoDiv.appendChild(formulario);
-        spanAñadir.classList.remove ("desaparecer");
+        nuevoDiv.style.display="inline-block";
     }
 });
+   
